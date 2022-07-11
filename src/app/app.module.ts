e45afcae7a0954a851d81fa701bfd79adb338846
Mykola Samila <samila.mykola@gmail.com>
2022-07-11 16:49:10 +0200

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsListComponent } from './news/news-list/news-list.component';
+import { NewsDetailComponent } from './news/news-detail/news-detail.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsListComponent,
+    NewsDetailComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
