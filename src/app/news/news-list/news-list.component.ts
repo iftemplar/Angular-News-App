e90c2fa087267b0a70b9fe7e64e603d355891091
Mykola Samila <samila.mykola@gmail.com>
2022-07-12 12:13:15 +0200

    Service and Model showing news on front

--- src/app/news/news-list/news-list.component.ts
+++ src/app/news/news-list/news-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {NewsService} from "../news.service";
+import {Article} from "../news.model";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-news-list',
@@ -7,11 +9,26 @@ import {NewsService} from "../news.service";
   styleUrls: ['./news-list.component.scss']
 })
 export class NewsListComponent implements OnInit {
+  news?: Article[];
+  newsSubscription?: Subscription;
 
   constructor(private newsService: NewsService) { }
 
   ngOnInit(): void {
-    this.newsService.fetchNews();
+     this.newsSubscription = this.newsService.fetchNews()
+      .subscribe(
+        (data) => {
+          this.news = data.articles;
+          console.log(this.news)
+        }
+      );
+  }
+
+  ngOnDestroy(): void {
+    if( this.newsSubscription) {
+      this.newsSubscription.unsubscribe()
+    }
   }
 
 }
+// Subscribe in service
---
+++ src/app/news/news.model.ts
@@ -0,0 +1,26 @@
+export interface Article {
+  author: string;
+  authors: string;
+  clean_url:string;
+  country: string;
+  is_opinion: boolean;
+  language: string;
+  link: string;
+  media: string;
+  published_date: string;
+  published_date_precision: string;
+  rank: number;
+  rights: string;
+  summary: string;
+  title: string;
+  topic: string;
+  twitter_account: string | null;
+  _id: string;
+  _score: number;
+}
+
+export interface ArticlesResponse {
+  articles: Article[];
+  status: string;
+  // Add more properties from response
+}
